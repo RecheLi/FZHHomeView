f166a029563f2e5c373eada5eacdb7798fbea1da
liht <liht@AppledeiMac.local>
2017-08-02 10:24:13 +0800

    Initial Commit

---
+++ 仿知乎网页版首页/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  仿知乎网页版首页
+//
+//  Created by liht on 2017/8/2.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
